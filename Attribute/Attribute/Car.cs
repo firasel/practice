ebfbd9493ec1d812212cf980a85c9c054025ea33
mdrasel <56898411+firasel@users.noreply.github.com>
2020-02-02 23:52:14 +0600

    add new file

---
+++ Attribute/Attribute/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attribute
+{
+    public class Car
+    {
+        //attributes
+        string _engine;
+        int _wheel;
+        int _seat;
+
+        //method
+        public void Start(int number)
+        {
+            if(number==1)
+                Console.WriteLine($"start");
+        }
+        public void Open()
+        { 
+            Console.WriteLine($"Open door");
+        }
+        public void Close()
+        {
+            Console.WriteLine($"close door");
+        }
+        public void Lock()
+        {
+            Console.WriteLine($"door locked");
+        }
+        public void Run()
+        {
+            Console.WriteLine($"run");
+        }
+        public void Break()
+        {
+            Console.WriteLine($"car break");
+        }
+        public void Unlock()
+        {
+            Console.WriteLine($"door unlock");
+        }
+
+        //default constructor
+        public Car(string engine,int wheel,int seat)
+        {
+            _engine = engine;
+            _seat = seat;
+            _wheel = wheel;
+        }
+
+    }
+}
---
+++ Attribute/Attribute/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("toyta",4,5);
+            Console.Write("start your car? enter:1");
+            int i = int.Parse(Console.ReadLine());
+            car.Start(i);
+            car.Open();
+            car.Close();
+            car.Lock();
+            car.Run();
+        }
+    }
+}
